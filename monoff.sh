4c9d3f63c239e34ed6345b29525233f34d050717
jonah <falsefriend13@gmail.com>
2022-01-16 11:14:37 -0500

    Updated monitor control to use vcgencmd

--- monoff.sh
+++ monoff.sh
@@ -6,5 +6,8 @@ killall fbi
 #wait 10 seconds
 sleep 10
 #
-#disconnect HDMI
-/opt/vc/bin/tvservice -o
+#turn off monitor using tvservice
+#/opt/vc/bin/tvservice -o
+
+#turn off monitor using vcgencmd
+vcgencmd display_power 0
